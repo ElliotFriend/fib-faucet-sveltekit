2799586097d73d77582a2396367802194a8db301
Elliot Voris <elliot@stellar.org>
2023-08-29 15:52:48 -0400

    Creating initial layouts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,20 @@
 <script>
     import "../app.postcss"
+
+    import Navbar from "$lib/components/Navbar.svelte"
+    import Footer from "$lib/components/Footer.svelte"
 </script>
 
-<slot />
+<div class="flex min-h-screen flex-col">
+
+    <Navbar />
+
+    <div class="grow">
+        <main class="prose max-w-full p-6">
+            <slot />
+        </main>
+    </div>
+
+    <Footer />
+
+</div>
